16bc0ef317586356bfab62cf17dd70cac3537336
“realBjornRoden” <realbjornroden@gmail.com>
2019-10-24 23:52:48 +0400

    new file:   hello.s

---
+++ hello/hello.s
@@ -0,0 +1,16 @@
+.section __DATA,__data
+str:
+  .asciz "Hello World!\n"
+
+.section __TEXT,__text
+.globl _main
+_main:
+  movl $0x2000004, %eax
+  movl $1, %edi
+  movq str@GOTPCREL(%rip), %rsi
+  movq $100, %rdx
+  syscall
+
+  movl $0, %ebx
+  movl $0x2000001, %eax           # exit 0
+  syscall
